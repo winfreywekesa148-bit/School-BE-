bd95746518b87ed9e9aff38d2c53b3f03a275f92
Winkesa <winfreywekesa148@gmail.com>
2026-07-28 13:35:16 +0300

    Create User model with database fields

---
+++ model/user.py
@@ -0,0 +1,8 @@
+from extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String, nullable=False)
